c6fdcf868cc403be23c119cad2bcddc3b69e8c52
Nathan Chan <nathanc@surveymonkey.com>
2016-02-12 21:16:07 -0800

    add home view

--- nuggets/urls.py
+++ nuggets/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    # url(r'^$', 'nuggets.views.home', name='home'),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'', include('nuggetsapp.urls')),
 ]
