cea84a9163de013cdb3d025da9481e2009ca1786
Aswath Krishnan <aswath87@gmail.com>
2016-03-20 20:07:07 -0400

    add model methods for daily review nuggets

--- nuggetsapp/models.py
+++ nuggetsapp/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+import datetime
+from datetime import timedelta
+
 
 class Nugget(models.Model):
     owner = models.ForeignKey('auth.User')
@@ -18,6 +21,10 @@ class Nugget(models.Model):
     @classmethod
     def get_nuggets_by_user(cls, user):
         return Nugget_User.get_nuggets_by_user(user)
+        
+    @classmethod
+    def get_todays_review_nuggets_by_user(cls, user):
+        return Nugget_User.get_todays_review_nuggets_by_user(user)
 
     @classmethod
     def create_new_nugget(cls, user, text, tags, source):
@@ -59,5 +66,21 @@ class Nugget_User(models.Model):
 
     @classmethod
     def get_nuggets_by_user(cls, user):
-        return [x.nugget for x in cls.objects.filter(user = user)]
+        return [x.nugget for x in cls.objects.filter(user = user).filter(is_deleted = False)]
+        
+    @classmethod
+    def get_todays_review_nuggets_by_user(cls, user):
+        today = datetime.date.today(); 
+        one_day_back = today - timedelta(days=1); 
+        three_days_back = today - timedelta(days = 3); 
+        one_week_back = today - timedelta(days = 7); 
+        two_weeks_back = today - timedelta(days = 14); 
+        one_month_back = today - timedelta(days = 30); 
+        three_months_back = today - timedelta(days = 3 * 30)
+        six_months_back = today - timedelta(days = 6 * 30)
+        one_year_back = today - timedelta(days = 12 * 30)
+        two_years_back = today - timedelta(days = 24 * 30)
+ 
+        return [x.nugget for x in  cls.objects.filter(Q(user = user) & Q(is_deleted = False) & (Q(created_at__date__gte = one_day_back) | Q(created_at__date__in = [three_days_back, one_week_back, two_weeks_back, one_month_back, three_months_back, six_months_back, one_year_back, two_years_back])))]
+     
 
