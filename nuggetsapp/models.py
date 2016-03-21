6a4608e1acde576443aad7d9e451c63ca8ea6ec7
Aswath Krishnan <aswath87@gmail.com>
2016-03-21 01:20:09 -0400

    remove stupid code

--- nuggetsapp/models.py
+++ nuggetsapp/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
-import datetime
-from datetime import timedelta
+from django.db.models import Q
+from utils import date_for_days_from_today
 
 
 class Nugget(models.Model):
@@ -66,21 +66,13 @@ class Nugget_User(models.Model):
 
     @classmethod
     def get_nuggets_by_user(cls, user):
-        return [x.nugget for x in cls.objects.filter(user = user).filter(is_deleted = False)]
-        
+        return [x.nugget for x in cls.objects.filter(Q(user = user) & Q(is_deleted = False))]
+                
     @classmethod
     def get_todays_review_nuggets_by_user(cls, user):
-        today = datetime.date.today(); 
-        one_day_back = today - timedelta(days=1); 
-        three_days_back = today - timedelta(days = 3); 
-        one_week_back = today - timedelta(days = 7); 
-        two_weeks_back = today - timedelta(days = 14); 
-        one_month_back = today - timedelta(days = 30); 
-        three_months_back = today - timedelta(days = 3 * 30)
-        six_months_back = today - timedelta(days = 6 * 30)
-        one_year_back = today - timedelta(days = 12 * 30)
-        two_years_back = today - timedelta(days = 24 * 30)
- 
-        return [x.nugget for x in  cls.objects.filter(Q(user = user) & Q(is_deleted = False) & (Q(created_at__date__gte = one_day_back) | Q(created_at__date__in = [three_days_back, one_week_back, two_weeks_back, one_month_back, three_months_back, six_months_back, one_year_back, two_years_back])))]
+        review_interval_days = [1, 3, 7, 14, 30, 90, 180, 360, 720]
+        review_dates = [date_for_days_from_today(n) for n in review_interval_days]
+        return [x.nugget for x in  cls.objects.filter(Q(user = user) & Q(is_deleted = False) & Q(created_at__date__in = review_dates))]
      
+    
 
---
+++ nuggetsapp/utils.py
@@ -0,0 +1,5 @@
+import datetime
+
+def date_for_days_from_today(dayCount): 
+        today = datetime.date.today(); 
+        return today - datetime.timedelta(days=dayCount);
