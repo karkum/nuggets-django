bf052416acfddde6b07337cd9e9b821559dc861d
Aswath Krishnan <aswath87@gmail.com>
2016-03-22 00:55:00 -0400

    fix method name in import

--- nuggetsapp/utils.py
+++ nuggetsapp/utils.py
@@ -1,6 +1,6 @@
 import datetime
 
-def date_for_days_before_today(dayCount): 
+def date_for_x_days_before_today(dayCount): 
         today = datetime.date.today() 
         return today - datetime.timedelta(days=dayCount)
         
